1c569d75744aab044ddcc3b174863643addc7b04
AshwaniRaii <ashwanirai975@gmail.com>
2024-12-28 11:35:51 +0530

    first-push

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/home.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1 class="h-screen">Welcome to Our Website</h1>
+   
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        'sans': ['Roboto', 'sans-serif'],
+                    },
+                }
+            }
+        }
+    </script>
+</head>
+<body>
+    @include('partials.header')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('partials.footer')
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
