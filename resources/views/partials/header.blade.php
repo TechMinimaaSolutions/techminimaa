3e8bf26daaf917241598d364c8140fbfaa346d74
AshwaniRaii <ashwanirai975@gmail.com>
2024-12-31 18:06:40 +0530

    ss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,8 @@
 <header class="py-1 px-4 sm:px-10 bg-blue-100 z-50 relative">
   <div class='max-w-7xl w-full mx-auto flex flex-wrap items-center gap-4'>
-    <a href="javascript:void(0)"><img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1735299218Screenshot__55_-removebg-preview.png" alt="logo" class='w-32' />
+    <a href="javascript:void(0)"><img
+        src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1735299218Screenshot__55_-removebg-preview.png" alt="logo"
+        class='w-32' />
     </a>
 
     <div id="collapseMenu"
@@ -19,7 +21,9 @@
       <ul
         class='lg:!flex lg:ml-12 lg:space-x-6 max-lg:space-y-6 max-lg:fixed max-lg:bg-white  w-[100%] max-lg:top-0 max-lg:left-0 max-lg:p-4 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
         <li class='max-lg:border-b max-lg:pb-4 px-3 lg:hidden'>
-          <a href="javascript:void(0)"><img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1735299218Screenshot__55_-removebg-preview.png" alt="logo" class='w-32' />
+          <a href="javascript:void(0)"><img
+              src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1735299218Screenshot__55_-removebg-preview.png"
+              alt="logo" class='w-32' />
           </a>
         </li>
         <li class='max-lg:border-b max-lg:py-2 px-3'>
@@ -82,15 +86,20 @@
     </div>
 
     <div class='flex ml-auto'>
-      <button
-        class='bg-blue-500 hover:bg-blue-700 hidden md:block text-white flex items-center transition-all font-semibold rounded-md px-5 py-3'>Get
-        started
+
+      <button href="/contact" id="contactBtn"
+        class="hidden md:flex items-center group relative px-8 py-2.5 bg-[#00e9c2] text-black font-semibold rounded-none hover:bg-white transition-all duration-300">
+        <span>Get Started</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="w-[14px] fill-current ml-2" viewBox="0 0 492.004 492.004">
           <path
             d="M484.14 226.886 306.46 49.202c-5.072-5.072-11.832-7.856-19.04-7.856-7.216 0-13.972 2.788-19.044 7.856l-16.132 16.136c-5.068 5.064-7.86 11.828-7.86 19.04 0 7.208 2.792 14.2 7.86 19.264L355.9 207.526H26.58C11.732 207.526 0 219.15 0 234.002v22.812c0 14.852 11.732 27.648 26.58 27.648h330.496L252.248 388.926c-5.068 5.072-7.86 11.652-7.86 18.864 0 7.204 2.792 13.88 7.86 18.948l16.132 16.084c5.072 5.072 11.828 7.836 19.044 7.836 7.208 0 13.968-2.8 19.04-7.872l177.68-177.68c5.084-5.088 7.88-11.88 7.86-19.1.016-7.244-2.776-14.04-7.864-19.12z"
             data-original="#000000" />
         </svg>
+        <div
+          class="absolute inset-0 border border-[#00e9c2] transform translate-x-1 translate-y-1 group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-300">
+        </div>
       </button>
+
       <button id="toggleOpen" class='lg:hidden ml-7'>
         <svg class="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd"
